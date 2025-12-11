66c86989b12f33503cb404f8d3aa11b6b3ba62cd
kdongsu5509 <kod66170@gmail.com>
2025-12-11 10:05:04 +0900

    fix: fix typo
    파일의 역할을 분명히 명시하기 위해 them -> theme으로 변경

--- lib/fcm/service/fcm_alert_permission_service_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:iamhere/fcm/service/fcm_alert_permission_service.dart';
-import 'package:riverpod_annotation/riverpod_annotation.dart';
-
-part 'fcm_alert_permission_service_provider.g.dart';
-
-@riverpod
-FcmAlertPermissionService fcmAlertPermissionService(Ref ref) {
-  return FcmAlertPermissionService();
-}
