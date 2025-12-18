117e1d15d509818efc5b8630960a5afcd2126c7c
kdongsu5509 <kod66170@gmail.com>
2025-12-18 09:06:49 +0900

    hotfix(issue18) : delete sms permission
    자체 sms 폐기에 따른 관련 권한 요청 로직 제거

--- lib/user_permission/view/user_permission_view.dart
+++ lib/user_permission/view/user_permission_view.dart
@@ -34,22 +34,16 @@ class _UserPermissionViewState extends ConsumerState<UserPermissionView> {
 
   @override
   Widget build(BuildContext context) {
-    // 1. 이제 이 값은 AsyncValue<List<PermissionItem>> 입니다.
     final asyncPermissionState = ref.watch(userPermissionViewModelProvider);
 
-    // 2. .when()을 사용하여 상태(로딩/에러/데이터)에 따라 분기 처리합니다.
     return asyncPermissionState.when(
-      // [로딩 중] 화면
       loading: () => const Scaffold(
         backgroundColor: Colors.white,
         body: Center(child: CircularProgressIndicator()),
       ),
-      // [에러 발생] 화면
       error: (error, stack) =>
           Scaffold(body: Center(child: Text('초기화 오류: $error'))),
-      // [데이터 로드 완료] 화면 (기존 로직은 여기로 들어옵니다)
       data: (permissionItems) {
-        // 데이터가 준비되었으므로, 기존 로직 수행
         int _totalPages = permissionItems.length + 2;
 
         return Scaffold(
@@ -73,8 +67,6 @@ class _UserPermissionViewState extends ConsumerState<UserPermissionView> {
                       } else if (index == _totalPages - 1) {
                         return FinishPage(onFinish: _onFinish);
                       } else {
-                        // permissionItems는 실제 List 데이터입니다.
-                        // index 계산 주의: 0번은 Intro, 1번부터 아이템이므로 (index - 1)
                         return PermissionPage(
                           pageIndex: _currentPage,
                           item: permissionItems[index - 1],
