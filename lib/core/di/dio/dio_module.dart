ddebfeb267e143c8312f6ddf91191df6c3e3fdca
kdongsu5509 <kod66170@gmail.com>
2026-01-12 21:29:49 +0900

    fix : delete duplicated code
    baseUrl를 중복으로 정의하여 초기화 시 문제 발생 -> 이를 해결하기 위해 사용하지 않는 코드 부분 제거하여 문제 해결함

--- lib/core/di/dio/dio_module.dart
+++ lib/core/di/dio/dio_module.dart
@@ -1,23 +1,23 @@
 import 'package:dio/dio.dart';
-import 'package:injectable/injectable.dart';
 import 'package:iamhere/auth/service/token_storage_service.dart';
+import 'package:injectable/injectable.dart';
+
 import 'dio_auth_interceptor.dart';
 import 'pending_request.dart';
 import 'token_refresher.dart';
 
 @module
 abstract class DioModule {
-  @Named("baseUrl")
-  String get serverUrl => throw UnimplementedError();
-
   @lazySingleton
   Dio dio(TokenStorageService tokenStorage, @Named("baseUrl") String url) {
-    final dio = Dio(BaseOptions(
-      baseUrl: url,
-      connectTimeout: const Duration(seconds: 10),
-      receiveTimeout: const Duration(seconds: 10),
-      headers: {'Content-Type': 'application/json'},
-    ));
+    final dio = Dio(
+      BaseOptions(
+        baseUrl: url,
+        connectTimeout: const Duration(seconds: 10),
+        receiveTimeout: const Duration(seconds: 10),
+        headers: {'Content-Type': 'application/json'},
+      ),
+    );
 
     final refresher = TokenRefresher(tokenStorage, url);
     final retrier = RequestRetrier()..setDio(dio);
